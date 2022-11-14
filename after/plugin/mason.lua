725b31b7a0490628abfc446d3bfadf56528fc08e
alealle <106605527+alealle@users.noreply.github.com>
2022-11-14 16:14:49 -0300

    updating all packages and plugins

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,33 +1,33 @@
 local status, mason = pcall(require, "mason")
 if not status then
-    return
+	return
 end
 local status2, lspconfig = pcall(require, "mason-lspconfig")
 if not status2 then
-    return
+	return
 end
 
 mason.setup({})
 
 lspconfig.setup({
-    ensure_installed = {
-        "arduino_language_server",
-        "autopep8",
-        "bashls",
-        "black",
-        "clangd",
-        "cmake",
-        "eslint_d",
-        "flake_8",
-        "jdtls",
-        "quick_lint_js",
-        "prettier",
-        "prettierd",
-        "pyright",
-        "sumneko_lua",
-        "sqlls",
-        "tailwindcss",
-        "tsserver",
-        "vimls",
-    },
+	ensure_installed = {
+		"arduino_language_server",
+		--"autopep8",
+		"bashls",
+		"clangd",
+		--"black",
+		"cmake",
+		--"flake_8",
+		--"eslint_d",
+		"jdtls",
+		"quick_lint_js",
+		--"prettier",
+		"pyright",
+		--"prettierd",
+		"sumneko_lua",
+		"sqlls",
+		"tailwindcss",
+		"tsserver",
+		"vimls",
+	},
 })
